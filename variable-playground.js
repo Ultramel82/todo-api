d29f535275bc6a0a73ca0485c87bed16d1aa9889
Melinda Nel <melinda@corpcam.com>
2016-09-26 15:11:06 +0200

    add variable-playground

---
+++ variable-playground.js
@@ -0,0 +1,32 @@
+// var person = {
+// 	name: 'Melinda',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name = 'Melinda',
+// 	// 	age = 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+// Array example
+var grades = [12, 88]
+
+//add grade that pushes on new value
+
+
+function addGrades(gradesArr) {
+	gradesArr.push(11);	
+	
+}
+
+addGrades(grades);
+console.log(grades)
+
+
