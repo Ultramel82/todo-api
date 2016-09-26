3a6b043d73f5fa2fee9520e8044e0ccc4ef56be9
Melinda Nel <melinda@corpcam.com>
2016-09-26 12:14:31 +0200

    Backup of server ; add post for todos

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},{
-	id: 3,
-	description: 'Fetch the kids from school',
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 //GET request /todos
 app.get('/todos', function(req, res) {
@@ -44,35 +36,19 @@ app.get('/todos/:id', function(req, res) {
 		res.status(404).send();
 	}
 
-	// for (var i = 0; i < todos.length - 1; i++) {
-	// 	if (todos[i].id === todoID) {
-	// 		matchedTodo = todos[i].id;
-	// 	}
-	// }
-
-	// if (typeof matchedTodo === 'undefined') {
-	// 	res.status(404).send();
-	// }
-	// else
-	// {
-	// 	res.json(todos[matchedTodo]);
-	// }
-
-
-	// todos.forEach(function (todo) {
-	// 	res.send(todos[todo]);
-	// 	// if (todo.id === todoID) {
-	// 	// 	res.send(todos[todo]);
-	// 	// 	//res.json(todos[todo]);
-	// 	// }
-	// 	// else
-	// 	// {
-	// 	// 	res.status(404).send();
-	// 	// }
-	// })
-
 });
 
+// POST - can take data
+// URL will be /todos but method will be different
+app.post('/todos', function(req, res) {
+	var body = req.body;
+
+	//add id field
+	body.id = todoNextId++;
+	//push body into array
+	todos.push(body);
+	res.json(body);
+});
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
